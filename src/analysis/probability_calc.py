31b866b96fcadbef382a0b46b6109598c12ecdf9
kosiarn <kosiarn@users.noreply.github.com>
2023-07-18 16:14:33 +0200

    format probability calc with black

--- src/analysis/probability_calc.py
+++ src/analysis/probability_calc.py
@@ -1,4 +1,7 @@
 from math import sqrt, exp, pi
 
+
 def getNormalDistribution(mu: float, sigma: float):
-    return lambda x: round(1/(sigma * sqrt(2*pi))* exp(-(1/2)*((x-mu)/sigma)**2), 5)
+    return lambda x: round(
+        1 / (sigma * sqrt(2 * pi)) * exp(-(1 / 2) * ((x - mu) / sigma) ** 2), 5
+    )
