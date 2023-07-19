f79a5939e46877fb730d42976695dcbe22ed7629
kosiarn <kosiarn@users.noreply.github.com>
2023-07-19 14:54:27 +0200

    fix distribution function formula

--- src/analysis/probability_calc.py
+++ src/analysis/probability_calc.py
@@ -12,5 +12,5 @@ def getNormalDistribution(distribution_parameters: tuple[float] | list[float]):
     mean = distribution_parameters[0]
     std = distribution_parameters[1]
     return lambda x: round(
-        1 / (mean * sqrt(2 * pi)) * exp(-(1 / 2) * ((x - mean) / std) ** 2), 5
+        1 / (std * sqrt(2 * pi)) * exp(-(1 / 2) * ((x - mean) / std) ** 2), 5
     )
