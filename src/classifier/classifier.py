cbf797389fdd7763fa1819229d560019c2e0c65a
kosiarn <kosiarn@users.noreply.github.com>
2023-07-31 20:00:41 +0200

    add working classifier

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,4 +1,4 @@
-from analysis.probability_calc import getNormalDistribution
+from analysis.probability_calc import getNormalDistribution, getPrior
 from data.numpy_processing_functions import *
 import pandas
 from pandas import DataFrame
@@ -18,6 +18,31 @@ def bayesClassifier(train: DataFrame, sample: List[float]) -> Tuple[any, dict]:
     - matched label,
     - dictionary of likelihoods for each label.
     """
-    if len(train) is not (len(train.iloc[0]) - 1):
-        return ValueError("Train dataset row(without label) and classfied sample vary in lengths")
-    
+    if len(sample) is not (len(train.iloc[0]) - 1):
+        raise ValueError(
+            "Train dataset row(without label) and classfied sample vary in lengths"
+            )
+    grouped_train = group_data_by_target(train)
+    prior = getPrior(train)
+    likelihoods = {}
+    distributions = {}
+    labels = grouped_train.keys()
+    for label in labels:
+        mean, std = summarize_dataset(grouped_train, label)
+        column_labels = mean.keys()
+        distribution_params = []
+        for column_label in column_labels:
+            distribution_params.append(
+                [mean[column_label], std[column_label]]
+            )
+        distributions[label] = distribution_params
+    for label in labels:
+        label_pdfs = []
+        for distribution_param in distributions[label]:
+            label_pdfs.append(getNormalDistribution(distribution_param))
+        label_likelihood = prior[label]
+        for feature, pdf in enumerate(label_pdfs):
+            label_likelihood = label_likelihood * pdf(sample[feature])
+        likelihoods[label] = label_likelihood
+    classification = max(likelihoods)
+    return (classification, likelihoods)
